84bed75e1e9ddf8d09187dba7ce8780bc2bc83d5
Timo Flesch <24828674+TimoFlesch@users.noreply.github.com>
2019-07-24 23:38:21 +0100

    pep8 (kinda)

--- code/main.py
+++ code/main.py
@@ -6,81 +6,114 @@ import time
 import random
 import numpy as np
 
+# window constants
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 1000
+
+# agent constants
+AGENT_WIDTH = 60
+AGENT_HEIGHT = 100
+
+AGENT_STARTX = WINDOW_WIDTH * .45
+AGENT_STARTY = WINDOW_HEIGHT * .75
+
+AGENT_SPEED = 5
+
+# image paths
+PATH_AGENTIMG = '../assets/spaceship.png'
+PATH_ASTEROIDIMG = '../assets/asteroid.png'
+
+# colors
+COL_WHITE = (255, 255, 255)
+COL_BLACK = (0, 0, 0)
+COL_YELLOW = (245, 206, 66)
+
+# obstacle constants
+OBSTACLE_WIDTH = 50
+OBSTACLE_HEIGHT = 50
+OBSTACLE_SPEED = 7
+
+# stars (background)
+N_STARS = 100
+STARS_STARTX = np.random.randint(0, WINDOW_WIDTH, N_STARS)
+STARS_STARTY = np.random.randint(0, WINDOW_HEIGHT, N_STARS)
+STARS_WIDTH = 2
+STARS_HEIGHT = 2
+STARS_SPEED = 2
+
+
 pygame.init()
-windowWidth = 800
-windowHeight = 1000
-gameDisplay = pygame.display.set_mode((windowWidth, windowHeight))
-pygame.display.set_caption('hello world')
+game_display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('asteriods 1.0')
 
 clock = pygame.time.Clock()
 
-agentImg = pygame.image.load('../assets/spaceship.png')
+agent_img = pygame.image.load(PATH_AGENTIMG)
 # make image smaller
-agentWidth = 60
-agentHeight = 100
-agentImg = pygame.transform.scale(agentImg, (agentWidth, agentHeight))
+agent_img = pygame.transform.scale(agent_img, (AGENT_WIDTH, AGENT_HEIGHT))
 
-asteroidImg = pygame.image.load('../assets/asteroid.png')
+asteroid_img = pygame.image.load(PATH_ASTEROIDIMG)
+
 
 def draw_agent(x, y):
-    gameDisplay.blit(agentImg, (x, y))
+    game_display.blit(agent_img, (x, y))
 
 
 def draw_stars(x, y, w, h):
     for ii in range(x.shape[0]):
-        pygame.draw.rect(gameDisplay, (245, 206, 66), [x[ii], y[ii], w, h])
+        pygame.draw.rect(game_display, COL_YELLOW, [x[ii], y[ii], w, h])
 
 
 def draw_obstacle(x, y, w, h, col):
-    # pygame.draw.rect(gameDisplay, col, [x, y, w, h])
-    gameDisplay.blit(asteroidImg, (x, y))
+    # pygame.draw.rect(game_display, col, [x, y, w, h])
+    game_display.blit(asteroid_img, (x, y))
+
 
 def draw_dodgecounter(count):
     font = pygame.font.SysFont(None, 26)
     text = font.render("Avoided Collisions: " + str(count),
-            True, (255, 255, 255))
-    gameDisplay.blit(text, (0, 0))
+                True, COL_WHITE)
+    game_display.blit(text, (0, 0))
 
 
 def update_stars(x, y):
     for ii in range(len(x)):
-        if y[ii] > windowHeight:
+        if y[ii] > WINDOW_HEIGHT:
             y[ii] = 0
-            x[ii] = np.random.randint(0,windowWidth,1)
+            x[ii] = np.random.randint(0, WINDOW_WIDTH, 1)
     return x, y
+
 
 def print_message(messageText):
     textFont = pygame.font.Font('freesansbold.ttf', 28)
-    textSurface = textFont.render(messageText, True, (255, 255, 255))
+    textSurface = textFont.render(messageText, True, COL_WHITE)
     textRect = textSurface.get_rect()
-    textRect.center = ((windowWidth/2, windowHeight/2))
+    textRect.center = ((WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 
-    gameDisplay.blit(textSurface, textRect)
+    game_display.blit(textSurface, textRect)
     pygame.display.update()
 
 
 def agent_left_screen(waitSecs=2):
     # print message
-    print_message('please stay insde the window!')
+    print_message('game over!')
     # wait for three seconds
     time.sleep(waitSecs)
     main_loop()
 
 
 def agent_crashed(waitSecs=2):
-    print_message('your ship got destroyed!')
+    print_message('game over!')
     time.sleep(waitSecs)
     main_loop()
 
 
-
 def main_loop():
     quitGame = False
-    agentXPos = windowWidth * .45
-    agentYPos = windowHeight * .75
+    agentXPos = AGENT_STARTX
+    agentYPos = AGENT_STARTY
 
-    spaceshipSpeed = 5
-    dodgedItems = 0
+    n_dodged = 0
 
     # define change of x and y (init to 0)
     delta_x = 0
@@ -89,17 +122,12 @@ def main_loop():
     # init obstacle
     obstacleXPos = random.randrange(0, agentXPos)
     obstacleYPos = -agentYPos
-    obstacleSpeed = 7
-    obstacleWidth = 50
-    obstacleHeight = 50
+    obstacleSpeed = OBSTACLE_SPEED
 
     # init stars
-    numStars = 100
-    starsXPos = np.random.randint(0,windowWidth,numStars)
-    starsYPos = np.random.randint(0,windowHeight,numStars)
-    starsWidth = 2
-    starsHeight = 2
-    starsSpeed = 2
+    starsXPos = STARS_STARTX
+    starsYPos = STARS_STARTY
+    starsSpeed = STARS_SPEED
 
     while not quitGame:
         for event in pygame.event.get():
@@ -112,13 +140,13 @@ def main_loop():
             if event.type == pygame.KEYDOWN:
                 # if left key, move left
                 if event.key == pygame.K_LEFT:
-                    delta_x = -spaceshipSpeed
+                    delta_x = -AGENT_SPEED
                 elif event.key == pygame.K_RIGHT:
-                    delta_x = spaceshipSpeed
+                    delta_x = AGENT_SPEED
                 elif event.key == pygame.K_UP:
-                    delta_y = -spaceshipSpeed
+                    delta_y = -AGENT_SPEED
                 elif event.key == pygame.K_DOWN:
-                    delta_y = spaceshipSpeed
+                    delta_y = AGENT_SPEED
                 elif event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     quit()
@@ -128,15 +156,15 @@ def main_loop():
                     delta_x = 0
                 elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     delta_y = 0
-            print(event)
-        gameDisplay.fill((0, 0, 0))
+            # print(event)
+        game_display.fill(COL_BLACK)
 
         # draw stars
-        draw_stars(starsXPos, starsYPos, starsWidth, starsHeight)
-        starsYPos+= starsSpeed
+        draw_stars(starsXPos, starsYPos, STARS_WIDTH, STARS_HEIGHT)
+        starsYPos += starsSpeed
         starsXPos, starsYPos = update_stars(starsXPos, starsYPos)
         # draw dodge counter
-        draw_dodgecounter(dodgedItems)
+        draw_dodgecounter(n_dodged)
 
         # update location of agent
         agentXPos += delta_x
@@ -145,30 +173,33 @@ def main_loop():
 
         # update location of obstacles
         draw_obstacle(obstacleXPos, obstacleYPos,
-            obstacleWidth, obstacleHeight, (255, 255, 255))
+                        OBSTACLE_WIDTH, OBSTACLE_HEIGHT, COL_WHITE)
         obstacleYPos += obstacleSpeed
 
-        # if obstacle has left screen, put it back on top, but with new random x location
-        if obstacleYPos > windowHeight:
-            obstacleYPos = 0-obstacleHeight
-            obstacleXPos = random.randrange(0, windowWidth)
+        # if obstacle has left screen, put it back on top, but
+        # with new random x location
+        if obstacleYPos > WINDOW_HEIGHT:
+            obstacleYPos = 0-OBSTACLE_HEIGHT
+            obstacleXPos = random.randrange(0, WINDOW_WIDTH)
             # count up score and increase difficulty of game
-            dodgedItems += 1
-            obstacleSpeed +=1
+            n_dodged += 1
+            obstacleSpeed += 1
             starsSpeed += 1
-            # obstacleWidth += (dodgedItems * 1.2)
+            # OBSTACLE_WIDTH += (n_dodged * 1.2)
 
         # if obstacle has touched agent, restart
-        if agentYPos < obstacleYPos+obstacleHeight:
-            print('y crossover')
+        if agentYPos < obstacleYPos+OBSTACLE_HEIGHT:            
             if not(agentYPos < obstacleYPos):
-                if (obstacleXPos + obstacleWidth > agentXPos and agentXPos > obstacleXPos
-                        or agentXPos+agentWidth > obstacleXPos and agentXPos+agentWidth < obstacleXPos+obstacleWidth):
-                        agent_crashed()
+                if (obstacleXPos + OBSTACLE_WIDTH > agentXPos and
+                        agentXPos > obstacleXPos or
+                        agentXPos+AGENT_WIDTH > obstacleXPos and
+                        agentXPos+AGENT_WIDTH < obstacleXPos +
+                        OBSTACLE_WIDTH):
+                    agent_crashed()
         # if agent has left screen, restart
         if (agentXPos < 0 or agentYPos < 0 or agentXPos
-                > windowWidth - agentWidth
-                or agentYPos > windowHeight - agentHeight):
+                > WINDOW_WIDTH - AGENT_WIDTH
+                or agentYPos > WINDOW_HEIGHT - AGENT_HEIGHT):
             agent_left_screen()
         pygame.display.update()
 
